2988614c7fd809c6c2c001ba5225bbb998db0ddd
matteotagliatti <matteotagliatti@gmail.com>
2022-05-04 15:16:02 +0200

    first commit. Structure done

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('templates.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1>Content</h1>
+@endsection
---
+++ resources/views/templates/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="it">
+    @include('partials.head')
+<body>
+    @include('partials.header')
+
+    @yield('content')
+
+    @include('partials.footer')
+</body>
+</html>
