4c9082cb70d901df0841114d333a5c3e70d460ed
matteotagliatti <matteotagliatti@gmail.com>
2022-05-04 15:28:20 +0200

    upload img, header done

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,15 @@
-<h1>Header</h1>
+<header class="container">
+    <img src="{{ asset('img/dc-logo.png')}}" alt="Logo" />
+    <ul>
+        <li><a href="#">characters</a></li>
+        <li><a class="active" href="#">comics</a></li>
+        <li><a href="#">movie</a></li>
+        <li><a href="#">tv</a></li>
+        <li><a href="#">games</a></li>
+        <li><a href="#">collectibles</a></li>
+        <li><a href="#">videos</a></li>
+        <li><a href="#">fans</a></li>
+        <li><a href="#">news</a></li>
+        <li><a href="#"">shop</a></li>
+    </ul>
+</header>
