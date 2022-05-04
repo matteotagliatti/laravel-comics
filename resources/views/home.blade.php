b4dd98fc7c56464eb13a624b5c1481d6dde376cd
matteotagliatti <matteotagliatti@gmail.com>
2022-05-04 15:37:52 +0200

    jumbo, cards container and link section done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,56 @@
 @section('title', 'Home')
 
 @section('content')
-    <h1>Content</h1>
+    <main>
+      <div class="jumbotron">
+        <img
+          class="jumbotron"
+          src="{{ asset('img/jumbotron.jpg')}}"
+          alt="Jumbotron"
+        />
+        <div class="container">
+          <a href="#">Current Series </a>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="cards">
+            {{-- ComicCard --}}
+          
+        </div>
+        <div class="load-more">
+          <a href="#">Load More</a>
+        </div>
+      </div>
+    </main>
+    {{-- LinkDiv --}}
+    <div id="links">
+        <div class="container">
+            {{-- Item --}}
+          <div >
+            <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="image" />
+            <span>Digital Comics</span>
+          </div>
+          {{-- Item --}}
+          <div >
+            <img src="{{ asset('img/buy-comics-merchandise.png') }}" alt="image" />
+            <span>DC Merchandise</span>
+          </div>
+          {{-- Item --}}
+          <div >
+            <img src="{{ asset('img/buy-comics-subscriptions.png') }}" alt="image" />
+            <span>Subscription</span>
+          </div>
+          {{-- Item --}}
+          <div >
+            <img src="{{ asset('img/buy-comics-shop-locator.png') }}" alt="image" />
+            <span>Comic Shop Locator</span>
+          </div>
+          {{-- Item --}}
+          <div >
+            <img src="{{ asset('img/buy-dc-power-visa.svg') }}" alt="image" />
+            <span>DC Power Visa</span>
+          </div>
+        </div>
+      </div>
 @endsection
